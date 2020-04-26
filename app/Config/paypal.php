e750ae0aff90d0c4d4ee62224c0d66af826d618a
Swaibu Rumbiiha <rumbiihas@gmail.com>
2020-04-26 17:01:36 +0300

    add paypal

--- app/Config/paypal.php
+++ app/Config/paypal.php
@@ -1,11 +1,33 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php namespace Config;
 
-/*
-It's highly recommended to use sandbox account for testing purpose
-*/
+use CodeIgniter\Config\BaseConfig;
 
-$config['mode']          = "sandbox"; //for live account use the key "live"
-$config['client_id']     = 'AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS'; //provide your client ID
-$config['client_secret'] = 'EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL'; //provide your secret ID
-$config['currency']      = 'USD'; //provide the currency code you want to use
+class Paypal extends BaseConfig
+{
+    public $client_id = 'AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS';
+    public $secret = 'EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL';
+    public $settings  = array(
+
+        'mode' => 'sandbox',
+        /**
+         * Specify the max request time in seconds
+         */
+        'http.ConnectionTimeOut' => 1000,
+        /**
+         * Whether want to log to a file
+         */
+        'log.LogEnabled' => true,
+        /**
+         * Specify the file that want to write on
+         */
+        'log.FileName' => 'application/logs/paypal.log',
+        /**
+         * Available option 'FINE', 'INFO', 'WARN' or 'ERROR'
+         *
+         * Logging is most verbose in the 'FINE' level and decreases as you
+         * proceed towards ERROR
+         */
+        'log.LogLevel' => 'FINE'
+    );
+
+}
---
+++ app/Models/PaypalModel.php
@@ -0,0 +1,28 @@
+<?php namespace App\Models;
+	use CodeIgniter\Model;
+
+class PaypalModel extends Model {
+
+	function __construct() {
+		parent::__construct();
+	}
+
+	/* This function create new Service. */
+
+	function create($Total,$SubTotal,$Tax,$PaymentMethod,$PayerStatus,$PayerMail,$saleId,$CreateTime,$UpdateTime,$State) {
+        $this->db->set('txn_id',$saleId);
+        $this->db->set('PaymentMethod',$PaymentMethod);
+        $this->db->set('PayerStatus',$PayerStatus);
+        $this->db->set('PayerMail',$PayerMail);
+        $this->db->set('Total',$Total);
+        $this->db->set('SubTotal',$SubTotal);
+        $this->db->set('Tax',$Tax);
+        $this->db->set('Payment_state',$State);
+		$this->db->set('CreateTime',$CreateTime);
+		$this->db->set('UpdateTime',$UpdateTime);
+		$this->db->insert('payments');
+		$id = $this->db->insert_id();
+		return $id;
+	}
+
+}
